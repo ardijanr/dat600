6b937c3c4bdd1b0a945955ac8d4fca42982a9e91
ardijanr <a.rexhaj@stud.uis.no>
2024-01-16 15:27:36 +0100

    attempted to fix pdf export, added insert sort

--- kernels.nix
+++ kernels.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
+  jupyterlab.runtimePackages = [pkgs.pandoc-cli];
   kernel.python.minimal = {
     enable = true;
+
   };
   kernel.rust."example".enable = true;
   kernel.c."example".enable = true;
